da7d3bb10e2f4888c75c4d6756361b248e13d27a
Kevin Yu <kevinyu567@gmail.com>
2026-04-04 14:36:24 -0400

    Link to home page in header

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link';
 export default function Header() {
     return (
         <header className="flex gap-4 items-center px-12 py-1 border-b border-tertiary">
-            <img
-                src="/assets/logo-uwu.png"
-                alt="logo"
-                className="h-16"
-            />
-            <h2 className="text-lg font-bold">b01lers CTF</h2>
+            <Link href="/" className="flex gap-3 items-center">
+                <img
+                    src="/assets/logo-uwu.png"
+                    alt="logo"
+                    className="h-16"
+                />
+                <h2 className="text-lg font-bold">b01lers CTF</h2>
+            </Link>
 
             <Link href="/challenges" className="px-2 text-primary hover:text-inherit transition duration-100">
                 Challenges
