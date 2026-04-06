c4b7917de11b3d833fd1b413cc4bd3a23d7bc995
Kevin Yu <kevinyu567@gmail.com>
2026-04-06 17:59:03 -0400

    Show timestamp, submissions in reverse chrono order

--- app/challenges/PreviousSubmissionsTable.tsx
+++ app/challenges/PreviousSubmissionsTable.tsx
@@ -21,7 +21,7 @@ export default function PreviousSubmissionsTable(props: PreviousSubmissionsTable
             const msg: SubmissionMessage = JSON.parse(d.data);
             if (msg.type === 'all') return setSubmissions(msg.submissions);
             if (msg.type === 'new')
-                return setSubmissions((s) => [msg.submission, ...s]);
+                return setSubmissions((s) => [...s, msg.submission]);
         }
         return () => events.close();
     }, [])
@@ -33,13 +33,16 @@ export default function PreviousSubmissionsTable(props: PreviousSubmissionsTable
                 <th className="px-2 w-12 text-right">#</th>
                 <th className="px-2 w-48 text-left font-semibold">Submission ID</th>
                 <th className="px-2 w-36 text-left font-semibold">Length</th>
-                <th className="px-2 text-left font-semibold">Score</th>
+                <th className="px-2 w-36 text-left font-semibold">Score</th>
+                <th className="px-2 text-left font-semibold">Time</th>
             </tr>
             </thead>
             <tbody>
-            {submissions.map((s, i) => (
+            {submissions.toReversed().map((s, i) => (
                 <tr key={s.id} className="bg-black/10">
-                    <td className="text-secondary text-right px-2">{i + 1}</td>
+                    <td className="text-secondary text-right px-2">
+                        {submissions.length - i}
+                    </td>
                     <td className="px-2 py-0.5 line-clamp-1">
                         <a
                             href={`/submission/${s.id}`}
@@ -50,6 +53,7 @@ export default function PreviousSubmissionsTable(props: PreviousSubmissionsTable
                     </td>
                     <td className="px-2">{s.body.length}</td>
                     <td className="px-2">...</td>
+                    <td className="px-2">{s.ts as unknown as string}</td> {/* TODO: fix serialization typing later */}
                 </tr>
             ))}
             </tbody>
