5d849654a2881bbaffe822d5a24721be0e957534
Kevin Yu <kevinyu567@gmail.com>
2026-04-17 16:28:21 -0400

    Deploy my epic hotfix for frontend!

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToastProvider from '@/components/ToastProvider';
 
 import './globals.css';
 import 'katex/dist/katex.min.css'
+import { redirect } from 'next/navigation';
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,6 +19,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout(props: Readonly<{ children: ReactNode }>) {
+    redirect('https://www.youtube.com/watch?v=CsCS5BOjdgk');
+
     return (
         <html
             lang="en"
