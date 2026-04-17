3f8ff5e21b2ff795aa0cdaf1a5c80792a8f13cf3
Kevin Yu <kevinyu567@gmail.com>
2026-04-17 14:08:52 -0400

    Fix scoreboard sorting

--- app/ChallengeScoreboard.tsx
+++ app/ChallengeScoreboard.tsx
@@ -9,20 +9,28 @@ import { BACKEND_URL } from '@/util/config';
 export default function ChallengeScoreboard(props: { id: string, className?: string }) {
     const [entries, setEntries] = useState<ScoreboardEntry[]>([]);
 
+    // Sort by primary score, then by secondary score
+    // TODO: display tertiary tiebreak on time
+    function sortEntries(a: ScoreboardEntry, b: ScoreboardEntry) {
+        const [a1, a2] = a.score;
+        const [b1, b2] = b.score;
+        return a2 - a1 || b2 - b1;
+    }
+
     useEffect(() => {
         const events = new EventSource(`${BACKEND_URL}/sse/scoreboard/${props.id}`);
 
         events.onmessage = (d) => {
             const msg: ScoreboardMessage = JSON.parse(d.data);
             switch (msg.type) {
-                case 'all': return setEntries(msg.entries);
+                case 'all': return setEntries(msg.entries.sort(sortEntries));
                 case 'new':
-                    return setEntries((s) => [...s, msg.entry]);
+                    return setEntries((s) => [...s, msg.entry].sort(sortEntries));
                 case 'update':
                     return setEntries((s) => {
                         const i = s.findIndex(s => s.id === msg.entry.id);
                         if (i === -1) return s;
-                        return s.with(i, msg.entry);
+                        return s.with(i, msg.entry).sort(sortEntries);
                     });
             }
         }
